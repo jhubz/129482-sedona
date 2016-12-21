8516f1af13efe1999fe810884add7153e53b0013
jhubz <jhubz@list.ru>
2016-12-21 22:05:22 +0300

    Исправил подключение picturefill.js, добавил на сайт скрытые блоки, доделал js на меню

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@ var mainMenu = document.querySelector(".main-nav");
 var mainMenuButton = document.querySelector(".page-header__nav-toggle");
 var logo = document.querySelector(".page-header__logo");
 
+mainMenu.classList.remove("main-nav--opened");
+mainMenu.classList.add("main-nav--closed");
+
+mainMenuButton.classList.remove("page-header__nav-toggle--close");
+mainMenuButton.classList.add("page-header__nav-toggle--open");
+
+logo.classList.remove("page-header__logo--opened-menu");
+logo.classList.add("page-header__logo--closed-menu");
+
 mainMenuButton.addEventListener("click", function (event) {
   event.preventDefault();
 
